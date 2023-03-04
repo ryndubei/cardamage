b9de1f610b9f2283e762af9fe4928bc4963593d6
ryndubei <114586905+ryndubei@users.noreply.github.com>
2023-03-04 21:45:01 +0000

    Make requests to API

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,30 @@
 module Main (main) where
 
-import CommandLine
+import Options.Applicative
+import qualified Data.ByteString as B
 import AnalyseDamage
+import Paths_cardamage
 
 main :: IO ()
 main = do
-  parts <- analyseImages =<< getImages
+  apiKey <- B.readFile =<< getDataFileName ".secrets/API_KEY.secret"
+  parts <- analyseImages apiKey =<< getImages
   mapM_ putStrLn parts
+
+data Parameters = Parameters { images :: [FilePath] }
+
+parameters :: Parser Parameters
+parameters = Parameters
+      <$> some (argument str 
+                  ( metavar "IMAGES..." 
+                 <> help "Images to analyse"
+                  )
+               ) 
+
+getImages :: IO [Image]
+getImages = execParser opts >>= \params -> mapM B.readFile (images params)
+  where
+    opts = info (parameters <**> helper)
+      ( fullDesc
+     <> progDesc "Return a list of broken car parts from a list of images"
+     <> header "haskell-cardamage")
--- src/AnalyseDamage.hs
+++ src/AnalyseDamage.hs
@@ -1,16 +1,69 @@
-module AnalyseDamage (analyseImages, Image) where
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+module AnalyseDamage where
 
-import Data.ByteString.Lazy (ByteString)
+import Data.ByteString (ByteString)
+import Network.HTTP.Simple (Request, parseRequest, Header, setRequestHeaders, setRequestBodyJSON, setRequestSecure, httpBS)
+import Data.Maybe (fromJust)
+import Data.Text (Text)
+import Data.Aeson (ToJSON (toEncoding), genericToEncoding, defaultOptions)
+import GHC.Generics (Generic)
+import Data.ByteString.Base64 (encodeBase64)
+import Network.HTTP.Client.Conduit (responseBody)
 
 type Image = ByteString
+type ApiKey = ByteString
 
 -- | Given a list of images, output a list of damaged car parts according
 -- to the image classification model.
-analyseImages :: [Image] -> IO [String]
-analyseImages imgs = mapM runModel imgs >>= \outputs ->
-  pure . map fst . filter (\(_,confidence) -> confidence >= 0.6) . concat $ outputs
+analyseImages :: ApiKey -> [Image] -> IO [String]
+analyseImages apiKey imgs = do
+  rawOutputs <- mapM (runModel apiKey) imgs
+  pure (map show rawOutputs)
 
-type Confidence = Double
+runModel :: ApiKey -> Image -> IO ByteString
+runModel apiKey image = do
+  response <- httpBS (apiRequest apiKey image)
+  pure (responseBody response)
 
-runModel :: Image -> IO [(String, Confidence)]
-runModel img = pure [("Windshield", 0.9), ("Door", 0.2), ("Lights", 0.6)]
+-- | Given an Image and the api key, construct an HTTP request to server
+-- hosting the AI model.
+apiRequest :: ApiKey -> Image -> Request
+apiRequest apiKey image = 
+  setRequestSecure True 
+  . setRequestBodyJSON body 
+  . setRequestHeaders headers 
+  $ baseRequest
+  where
+    baseRequest = fromJust $ parseRequest "POST https://vehicle-damage-assessment.p.rapidapi.com/run"
+    headers = 
+      [ ("content-type", "application/json")
+      , ("X-RapidAPI-Key", apiKey)
+      , ("X-RapidAPI-Host", "vehicle-damage-assessment.p.rapidapi.com")] :: [Header]
+    body = ApiPayload 
+      { draw_result = False 
+      , remove_background = False 
+      , image = encodeBase64 image 
+      }
+
+testImage :: Image
+testImage = ""
+
+data ApiPayload = ApiPayload 
+  { draw_result :: Bool
+  , remove_background :: Bool
+  , image :: Text -- Base64-encoded
+  } deriving (Generic, Show)
+
+instance ToJSON ApiPayload where
+  toEncoding = genericToEncoding defaultOptions
+
+-- | Output for specifically
+data ApiOutput = ApiOutput
+  { bbox :: [Int]
+  , damage_category :: String
+  , damage_color :: [Int]
+  , damage_id :: String
+  , damage_location :: String
+  , score :: String
+  }
